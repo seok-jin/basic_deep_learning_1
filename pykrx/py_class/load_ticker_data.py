c7456bb07d90b88f4f1418d4df7072965a6567da
SeokJin.Lim <hopesukjin@gmail.com>
2024-03-15 17:03:24 +0900

    .

---
+++ pykrx/py_class/load_ticker_data.py
@@ -0,0 +1,65 @@
+import pandas as pd 
+
+import sys,os
+sys.path.append(os.pardir)
+from py_class.date_convert import StockDay, DateConverter
+
+from pykrx import stock
+from pykrx import bond
+
+ticker_dir = './data/ticker_list.csv'
+
+
+class load_ticker_list:
+    def __init__(self) -> None:
+        if os.path.isfile(ticker_dir) :
+            self.df_ticker = pd.read_csv(ticker_dir)  
+        else:
+            tickers = stock.get_market_ticker_list(market='KOSPI')
+            # print(tickers)
+            # tickers = np.array(tickers)
+            data = { 
+                "ticker" : tickers
+            }
+            df_tickers = pd.DataFrame(data)
+            # tickers_len = tickers.shape[0]
+            ticker_name = []
+    
+            for i in range(len(data["ticker"])):
+                ticker_name.append(stock.get_market_ticker_name(data["ticker"][i]))
+    
+            df_tickers.insert(1,"ticker_name",ticker_name,True)
+            df_tickers.to_csv(ticker_dir)
+ 
+            self.df_ticker = df_tickers
+
+
+    
+
+
+
+
+
+
+
+def get_market_ohlcv_to_csv(sdt,edt,ticker): 
+    if os.path.isfile("./data/"+ticker+".csv"):
+        # 기존에 있는 파일 
+        if get_last_ticker_date(edt,ticker):
+            print("종료")
+        else:
+            df = stock.get_market_ohlcv(edt,edt,ticker)
+            df.to_csv("./data/"+ticker+".csv",mode='a', header=False)
+    else: #신규
+        df = stock.get_market_ohlcv(sdt,edt,ticker)
+        df.to_csv("./data/"+ticker+".csv", header=True)
+    
+def get_last_ticker_date(edt,ticker):
+    df_asis = pd.read_csv("./data/"+ticker+".csv")
+    if df_asis.iloc[-1]['날짜'].replace('-','') == edt:
+        return True
+    else : 
+        return False
+    del df_asis
+
+# df = stock.get_market_ohlcv("19000101", "20240314", "079980")
--- pykrx/py_lib/krx_lib.py
+++ pykrx/py_lib/krx_lib.py
@@ -1,14 +1,12 @@
 from datetime import datetime as tmdt
 from datetime import timedelta as dtmt
-import time
-import sys, os
+import time, sys, os
+import pandas as pd 
 sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 
-from pykrx import stock
-from pykrx import bond
-# import numpy as np
-import pandas as pd 
-# import datetime as dt 
+# external package
+# from pykrx import stock
+# from pykrx import bond
 
-
+# user package
 from py_class.date_convert import DateConverter,StockDay
