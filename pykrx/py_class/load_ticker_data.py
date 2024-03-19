316d54a26e36ec1781084b645e2831d48ab71e77
SeokJin.Lim <hopesukjin@gmail.com>
2024-03-19 11:03:45 +0900

    .

--- pykrx/py_class/load_ticker_data.py
+++ pykrx/py_class/load_ticker_data.py
@@ -4,8 +4,15 @@ import sys,os
 sys.path.append(os.pardir)
 from py_class.date_convert import StockDay, DateConverter
 
-from pykrx import stock
-from pykrx import bond
+# from pykrx import stock
+# from pykrx import bond
+
+
+from py_lib.krx_lib import *
+# from py_class.load_ticker_data import load_ticker_list
+# old= load_ticker_list()
+# old_ticker = old.df_ticker
+
 
 ticker_dir = './data/ticker_list.csv'
 
@@ -40,13 +47,38 @@ class load_ticker_list:
  
             self.df_ticker = df_tickers
 
+    def batch_ticker_list(self):
+        old_ticker = self.df_ticker
+        tickers = stock.get_market_ticker_list(market='KOSPI')
+        # print(tickers)
+        # tickers = np.array(tickers)
+        data = { 
+            "ticker" : tickers
+        }
+        new_tickers = pd.DataFrame(data)
+        # tickers_len = tickers.shape[0]
+        ticker_name = []
+
+        for i in range(len(data["ticker"])):
+            ticker_name.append(stock.get_market_ticker_name(data["ticker"][i]))
+
+        new_tickers.insert(1,"ticker_name",ticker_name,True)
+        # new_tickers.to_csv(ticker_dir)
+        changes  = new_tickers[new_tickers['ticker'].isin(old_ticker['ticker'])]
+        changes = changes[changes['ticker_name'] != old_ticker['ticker_name']]
+
+        # old_ticker[old_ticker['ticker']=='001045',ticker_name] = 'CJ대한통운'
+        old_ticker.loc[old_ticker['ticker'].isin(changes['ticker']),'comment'] = old_ticker['ticker_name']+' -> '+changes['ticker_name']
+        old_ticker.loc[old_ticker['ticker'].isin(changes['ticker']),'ticker_name'] = changes['ticker_name']
+        old_ticker.loc[old_ticker['ticker'].isin(changes['ticker']),'update_dt'] = DateConverter.convert_to_dash_format(StockDay.get_now_date())
+
+        ticker_dir = './data/ticker_list.csv'
+
+        old_ticker.to_csv(ticker_dir)
+        self.df_ticker = old_ticker
+
 
     
-
-
-
-
-
 
 
 def get_market_ohlcv_to_csv(sdt,edt,ticker): 
