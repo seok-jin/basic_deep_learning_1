2cf82ec0fd335b46de17cc8c84d9861f5cb5416d
SeokJin.Lim <hopesukjin@gmail.com>
2024-03-19 11:18:42 +0900

    .

--- pykrx/py_lib/krx_lib.py
+++ pykrx/py_lib/krx_lib.py
@@ -4,6 +4,7 @@ import time, sys, os
 import pandas as pd 
 sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 
+import logging
 # external package
 from pykrx import stock
 from pykrx import bond
