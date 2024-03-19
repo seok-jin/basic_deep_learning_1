2730a5aad86ef6e797662aa1bd2d6e3bddedef75
SeokJin.Lim <hopesukjin@gmail.com>
2024-03-19 13:02:52 +0900

    .

--- pykrx/read_ticker.py
+++ pykrx/read_ticker.py
@@ -1,23 +1,2 @@
-from pykrx import stock
-from pykrx import bond
-# import numpy as np
-import pandas as pd 
-import os.path  
-import datetime as dt 
-
-tickers = stock.get_market_ticker_list(market='KOSPI')
-# print(tickers)
-# tickers = np.array(tickers)
-data = { 
-    "ticker" : tickers
-}
-df_tickers = pd.DataFrame(data)
-# tickers_len = tickers.shape[0]
-ticker_name = []
-
-for i in range(len(data["ticker"])):
-    ticker_name.append(stock.get_market_ticker_name(data["ticker"][i]))
-
-df_tickers.insert(1,"ticker_name",ticker_name,True)
-
-# df_market_ohlcv = stock.get_market_ohlcv("20240314")
+from py_class.load_ticker_data import load_ticker_list
+load_ticker_list().batch_ticker_list()
